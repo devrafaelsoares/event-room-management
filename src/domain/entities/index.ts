8aacd1fc9d7dd7ce003aca91d47b0287c63c599e
devrafaelsoares <rafael.soares.developer@gmail.com>
2024-07-22 11:44:26 -0300

    🎉 feat: centralize the import of new modules

--- src/domain/entities/index.ts
+++ src/domain/entities/index.ts
@@ -1,2 +1,7 @@
 export * from './user';
 export * from './user-role';
+export * from './booking';
+export * from './room';
+export * from './event';
+export * from './email';
+export * from './booking-status';
